e370a42ff99b64e5567ac35b4ffbdef28ff028cd
Gaspela04 <samirmillanorozco@hotmail.com>
2019-09-18 20:11:52 +0000

    C cmms

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+/**
+ * main - print the size of char, int, long int, long long int, and float
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+int main(void)
+{
+printf("Size of a char: %i byte(s)\n", (int) sizeof(char));
+printf("Size of an int: %i byte(s)\n", (int) sizeof(int));
+printf("Size of a long int: %i byte(s)\n", (int) sizeof(long int));
+printf("Size of a long long int: %i byte(s)\n", (int) sizeof(long long int));
+printf("Size of a float: %i byte(s)\n", (int) sizeof(float));
+return (0);
+}
