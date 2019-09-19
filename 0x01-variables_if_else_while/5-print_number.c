0453ee6d763bcb2fe17a6a091bfb573ac6c64e68
Gaspela04 <samirmillanorozco@hotmail.com>
2019-09-19 21:18:17 +0000

    Ifelsew

---
+++ 0x01-variables_if_else_while/5-print_number.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints all single digit numbers of base 10 starting
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	printf("%i", n);
+
+	putchar('\n');
+
+	return (0);
+}
