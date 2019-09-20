9d2cbea02cb938b8e8ad9ac72b8b671d8bf9a0fe
Gaspela04 <samirmillanorozco@hotmail.com>
2019-09-20 00:21:44 +0000

    Ifelsew

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Advanced combinations of two digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int two;
+	int one;
+
+	for (one = 0; one <= 9; one++)
+	{
+	for (two = one + 1; two <= 9; two++)
+	{
+	putchar(two + '0');
+	putchar(one + '0');
+	if (two < 8)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
