8ee6773f41bdbab3dfddd00345c946763c50e9c4
Gaspela04 <samirmillanorozco@hotmail.com>
2019-09-20 03:16:05 +0000

    Ifelsew

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - All possible combinations of two two-digit numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 0; n < 100; n++)
+	{
+	for (m = 0; m < 100; m++)
+	{
+	if ((n == 0 && m != 0) || ( > n))
+	{
+	putchar(n / 10 + '0');
+	putchar(n % 10 + '0');
+	putchar(' ');
+	putchar(m / 10 + '0');
+	putchar(m % 10 + '0');
+	if (n < 98)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
