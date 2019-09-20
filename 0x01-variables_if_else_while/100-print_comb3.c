c38718bdebd496d18d3b42405f7514c9e6a6568d
Gaspela04 <samirmillanorozco@hotmail.com>
2019-09-20 00:31:51 +0000

    Ifelsew

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,15 +6,15 @@
  */
 int main(void)
 {
-	int two;
 	int one;
+	int two;
 
 	for (one = 0; one <= 9; one++)
 	{
 	for (two = one + 1; two <= 9; two++)
 	{
-	putchar(two + '0');
 	putchar(one + '0');
+	putchar(two + '0');
 	if (one < 8)
 	{
 	putchar(',');
