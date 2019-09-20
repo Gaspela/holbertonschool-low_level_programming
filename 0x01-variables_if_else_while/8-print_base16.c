724927c39511ffdfbce2c15ff932c7c163a78da8
Gaspela04 <samirmillanorozco@hotmail.com>
2019-09-20 00:08:09 +0000

    Ifelsew

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main -  Prints all the numbers of base 16 in lowercase, followed by a new line.
+ * main -  Prints base 16 in lowercase, followed by a new line.
  *
  * Return: Always 0 (Success)
  */
