8645b27d7ae2b22fbe9f3120de6951de0c7cac2c
Gaspela04 <samirmillanorozco@hotmail.com>
2019-09-24 13:52:16 +0000

    Loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,7 @@
 #include "holberton.h"
 /**
- * main - Entry point
+ * print_alphabet_x10 - Prints 10 the alphabet, lowerc, a new line.
  *
- * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
 {
