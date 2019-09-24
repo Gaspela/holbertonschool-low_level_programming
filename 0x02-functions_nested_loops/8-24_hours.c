57685620b287e0ce6b47e402348e82b588fa46eb
Gaspela04 <samirmillanorozco@hotmail.com>
2019-09-24 16:32:53 +0000

    Loops

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+  * jack_bauer - Prints the hour and min.
+  *
+  * Return: 0
+ */
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar(h / 10 + '0');
+			_putchar(h % 10 + '0');
+			_putchar(':');
+			_putchar(m / 10 + '0');
+			_putchar(m % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
