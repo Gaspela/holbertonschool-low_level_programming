c631e7a222cea2d8c07fef4abaf6308ea0ecdb97
Gaspela04 <samirmillanorozco@hotmail.com>
2019-09-25 02:54:57 +0000

    Loops

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,50 @@
+#include "holberton.h"
+/**
+ * print_times_table - Prints the n times table, starting with 0.
+ * 
+ * @n: Vaule data check
+ */
+
+void print_times_table(int n)
+{
+	int m;
+	int t;
+
+	if ((n > 15) || (n < 0))
+		return;
+
+	for (m = 0; m <= n; m++)
+	{
+		for (t = 0; t <= n; t++)
+		{
+			if ((m * t) < 10)
+			{
+				if (t != 0)
+				{
+				_putchar(' ');
+				_putchar(' ');
+				}
+			}
+			else if ((m * t) < 100)
+			{
+				_putchar(' ');
+				_putchar(m * t / 10 + '0');
+			}
+			else
+			{
+				_putchar(m * t / 100 + '0');
+				_putchar((m * t / 10) % 10 + '0');
+			}
+				_putchar(m * t % 10 + '0');
+			if (m == t)
+			{
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+}
