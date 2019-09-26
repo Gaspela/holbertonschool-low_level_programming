31d7c3fd799618b683405eac9f47840f7a11f59e
Gaspela04 <samirmillanorozco@hotmail.com>
2019-09-26 18:12:30 +0000

    loops2

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - Function that prints more the numbers, from 0 to 9.
+ * Return: null
+ *
+*/
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i = <= 9; i++)
+	{
+		if ((i != '2') && (i != '4'))
+			_putchar ('i');
+	}
+}
