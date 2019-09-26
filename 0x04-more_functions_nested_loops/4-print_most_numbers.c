9ca2f4c68033c8345ec554e2f1148ea57c77dbdb
Gaspela04 <samirmillanorozco@hotmail.com>
2019-09-26 18:51:42 +0000

    Loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,10 +8,11 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = '0'; i = <= 9; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		if ((i != '2') && (i != '4'))
-			_putchar ('i');
+		if (i != '2' && i != '4')
+			_putchar(i);
 	}
+
 			_putchar('\n');
 }
