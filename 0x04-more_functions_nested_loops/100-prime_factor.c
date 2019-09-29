4cd0a3900d809597e837b2766257e34db96b6558
Gaspela04 <samirmillanorozco@hotmail.com>
2019-09-29 18:14:08 +0000

    Loops2

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,4 +1,3 @@
-#include "holberton.h"
 #include <stdio.h>
 /**
  * main - Prime factor of the number 612852475143.
@@ -6,7 +5,7 @@
  */
 int main(void)
 	{
-	long int a = 612852475143;
+	long int m = 612852475143;
 	int n = 2;
 
 	while (m > n)
