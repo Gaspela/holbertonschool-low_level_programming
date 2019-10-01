4329a99fccd1ba9904f7bd8ae35d86e037a1fe46
Gaspela04 <samirmillanorozco@hotmail.com>
2019-10-01 19:01:11 +0000

    Pointer

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "holberon.h"
+/**
+ * _strlen - Return the length of a string.
+ * @s: Check value string.
+ * Return: lnt
+ */
+int _strlen(char *s)
+{
+	int i, lnt;
+
+	lnt = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		lnt++;
+	{
+	return (lnt);
+}
