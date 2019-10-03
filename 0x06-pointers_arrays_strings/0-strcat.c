f38d605b9d5299b6eca6dd24f851481c4c5d8294
Gaspela04 <samirmillanorozco@hotmail.com>
2019-10-03 18:36:29 +0000

    Pointer2

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,25 +3,26 @@
  * _strcat - Funtion string.
  * @dest: destiny value.
  * @src: String value.
- * Return: String.
+ * Return: dest.
 */
 char *_strcat(char *dest, char *src)
 {
 	int i, src_lnt, dest_lnt;
-/* src 12 */
+
 	src_lnt = 0;
-	for (i = 0; src[i] != '\0'; i++)
-		src_lnt++;
-/* dest 1 */
+		for (i = 0; src[i] != '\0'; i++)
+			src_lnt++;
+
 	dest_lnt = 0;
-	for (i = 0; dest[i] != '\0'; i++)
-		dest_lnt++;
-/* Print src dest */
-	for (i = 0; i <= src_lnt; i++)
-		dest[dest_lnt + i] = src[i];
+		for (i = 0; dest[i] != '\0'; i++)
+			dest_lnt++;
+
+		for (i = 0; i <= src_lnt; i++)
+			dest[dest_lnt + i] = src[i];
 
 
 		return (dest);
+
 }
 
 
