984aebe061b864e0f57ebc8e1d3de2df7bb71d84
Gaspela04 <samirmillanorozco@hotmail.com>
2019-10-03 21:39:58 +0000

    Pointer2

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * _reverse_array - Funtion string.
+ * reverse_array - Funtion string.
  * @a: Array reverse
  * @n: Elements
 */
@@ -9,11 +9,12 @@ void reverse_array(int *a, int n)
 	int i, j, tmp;
 
 	j = n;
-	for (i = 0; i < j; i++)
-	{
+		for (i = 0; i < j; i++)
+		{
 		tmp = a[i];
 		a[i] = a[n - i - 1];
 		a[n - i - 1] = tmp;
 		j--;
-	}
+	
+		}
 }
