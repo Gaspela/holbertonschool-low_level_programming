60b0b8a18446e983db894e73b170ac76822eda88
Gaspela04 <samirmillanorozco@hotmail.com>
2019-10-03 18:30:52 +0000

    Pointer2

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,20 +8,20 @@
 char *_strcat(char *dest, char *src)
 {
 	int i, src_lnt, dest_lnt;
-
+/* src 12 */
 	src_lnt = 0;
-	/* Print src */
 	for (i = 0; src[i] != '\0'; i++)
 		src_lnt++;
-		/* Print dest */
+/* dest 1 */
 	dest_lnt = 0;
-		for (i = 0; dest[i] != '\0'; i++)
-			dest_lnt++;
-			/* Print dest src */
-			for (i = 0; i <= src_lnt; i++)
-				dest[dest_lnt + i] = src[i];
+	for (i = 0; dest[i] != '\0'; i++)
+		dest_lnt++;
+/* Print src dest */
+	for (i = 0; i <= src_lnt; i++)
+		dest[dest_lnt + i] = src[i];
 
-	return (dest);
+
+		return (dest);
 }
 
 
