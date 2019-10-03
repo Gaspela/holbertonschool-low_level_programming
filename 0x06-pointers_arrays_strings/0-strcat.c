6516a9447bb86bf65e9eaa9a979758246b26ce5a
Gaspela04 <samirmillanorozco@hotmail.com>
2019-10-03 18:40:56 +0000

    Pointer2

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,4 @@
-#include "holberton"
+#include "holberton.h"
 /**
  * _strcat - Funtion string.
  * @dest: destiny value.
@@ -8,15 +8,15 @@
 char *_strcat(char *dest, char *src)
 {
 	int i, src_lnt, dest_lnt;
-
+/* src 12 */
 	src_lnt = 0;
 		for (i = 0; src[i] != '\0'; i++)
 			src_lnt++;
-
+/* dest 1 */
 	dest_lnt = 0;
 		for (i = 0; dest[i] != '\0'; i++)
 			dest_lnt++;
-
+/* print dest */
 		for (i = 0; i <= src_lnt; i++)
 			dest[dest_lnt + i] = src[i];
 
