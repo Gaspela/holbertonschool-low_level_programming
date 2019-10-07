f3b3f7ffc1246615b1b4461a3f6c083092ae0f9b
Gaspela04 <samirmillanorozco@hotmail.com>
2019-10-07 20:47:28 +0000

    Pointer3

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,12 +10,11 @@ char *_strchr(char *s, char c)
 {
 	int i;
 /*Scroll the arrangement and find char c*/
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0' && s[i] != c; i++)
 /*Find and compare char c*/
 		if (s[i] == c)
-			{
 			return (&s[i]);
-			}
+			
 	return ('\0');
 
 }
