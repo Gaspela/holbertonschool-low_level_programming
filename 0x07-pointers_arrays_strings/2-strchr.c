5b78b2e031976e2e73b43b14e9f07ea6b9c1d7db
Gaspela04 <samirmillanorozco@hotmail.com>
2019-10-07 20:22:11 +0000

    Pointer3

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _strchr - To locate one char c.
+ * @s: Array locate string.
+ * @c: Value c.
+ *
+ * Return: Null.
+*/
+char *_strchr(char *s, char c)
+{
+	int i;
+/*Scroll the arrangement and find char c*/
+	for (i = 0; s[i] != '\0' && s[i] != c; i++)
+/*Find and compare char c*/
+		if (s[i] == c)
+			return (&s[i]);
+	return ('\0');
+
+}
