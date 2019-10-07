bb783f44d162b03f440d3b09eb58f4151380ee20
Gaspela04 <samirmillanorozco@hotmail.com>
2019-10-07 18:59:44 +0000

    Pointer3

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,5 +1,21 @@
 #include "holberton.h"
+/*
+ * _memcpy - Copy memori area.
+ * @n: Value numbers copy memory.
+ * @dest: Destiny array copy.
+ * @src: Source copy.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
 
-#include "holberton.h"
+	for (i = 0; i < n; i++)
+		{
+		dest[i] = src[i];
+		}
+
+	return (dest);
+
+}
 
 
