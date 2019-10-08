1f3032bf424d54959b8bd417b682d5f466fa5cb6
Gaspela04 <samirmillanorozco@hotmail.com>
2019-10-08 02:18:18 +0000

    Pointer3

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * _strspn - Gets the length of a prefix substring.
+ * _strpbrk - Searches a string for any of a set of bytes.
  * @s: String find value.
  * @accept: Bytes to search.
  *
@@ -13,13 +13,14 @@ char *_strpbrk(char *s, char *accept)
 	int j;
 
 	for (i = 0; s[i] != '\0'; i++)
-		{
+	{
 		for (j = 0; accept[j] != '\0'; j++)
-			{
-			if (s[i] == accept[j])
-					return (&s[i]);
-			}
-		}
+		{
 
-	return (0);
+			if (s[i] == accept[j])
+				return (&s[i]);
+		}
+	}
+
+		return (0);
 }
