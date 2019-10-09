252b32d6495a5d8de827cc37ace8ca54ff68eae8
Gaspela04 <samirmillanorozco@hotmail.com>
2019-10-09 03:01:17 +0000

    Pointer3

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,19 +9,19 @@
 char *_strstr(char *haystack, char *needle)
 {
 	int i;
-	int j; 
+	int j;
 	int lnt;
 
 	for (i = 0; haystack[i] != '\0'; i++)
-	{
-		for (lnt = i, j = 0; needle[j] != '\0'; lnt++, j++)
 		{
+		for (lnt = i, j = 0; needle[j] != '\0'; lnt++, j++)
+			{
 			if (needle[j] != haystack[lnt] || haystack[lnt] == '\0')
-					break;
-		}
-		if (needle[j] == '\0')
+				break;
+			}
+				if (needle[j] == '\0')
 
-			return (haystack + i);
-	}
-	return ('\0');
+				return (haystack + i);
+		}
+		return ('\0');
 }
