fe4a958b0b57f9dccb36653ac3807e3227f3c228
Gaspela04 <samirmillanorozco@hotmail.com>
2019-10-11 04:03:40 +0000

    Recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+/**
+ * is_prime_number - Returns 1 if the input integer is a prime number.
+ * @n: Value n
+ * Return: prime
+ */
+int is_prime_number(int n)
+{
+
+	return (prime(2, n));
+
+}
+
+/**
+ * prime - Return if a number is primary or not.
+ * @i: Value ++.
+ * @n: Value check.
+ * Return: 1 and 0.
+ */
+
+int prime(int i, int n)
+{
+
+	if (n <= 1)
+
+		return (0);
+	else if (i >= n)
+		return (1);
+
+	if (n % i != 0 && i != n)
+
+		return (prime(i + 1, n));
+	else
+		return (0);
+
+}
