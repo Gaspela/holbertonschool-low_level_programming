85fba55d9ec7d765e106886cdfb1fe3fd2088921
Gaspela04 <samirmillanorozco@hotmail.com>
2019-10-11 03:28:48 +0000

    Recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,12 +7,7 @@
 int _sqrt_recursion(int n)
 {
 
-	if (n < 0)
-		return (-1);
-		else if (n == 0)
-			return (n);
-
-	return (sqrt(n, 1));
+	return (sqrt(0, n));
 
 }
 /**
@@ -21,14 +16,17 @@ int _sqrt_recursion(int n)
  * @i: Value number.
  * Return: -1.
  */
-int sqrt(int n, int i)
+int sqrt(int i, int n)
 {
 
-		if (i * i > n)
-			return (-1);
-		if (i * i == n)
-			return (i);
-		else
-			return (sqrt(n, i + 1));
+	if (i * i == n)
+
+		return (i);
+	else if (i < n)
+		{
+		return (sqrt(i + 1, n));
+		}
+	else
+		return (-1);
 
 }
