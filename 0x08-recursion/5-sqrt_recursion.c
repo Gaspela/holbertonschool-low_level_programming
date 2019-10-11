4cb76da586d772d5e46a3ab3f6591916cf3b402c
Gaspela04 <samirmillanorozco@hotmail.com>
2019-10-11 03:37:06 +0000

    Recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,30 +1,30 @@
 #include "holberton.h"
 /**
- * _sqrt_recursion - Returns the natural square root of a number.
- * @n: Number.
- * Return: Mitad.
- */
+  * _sqrt_recursion - Call the function that calculates the sqrt
+  * @n: Value
+  * Return: Value
+  */
 int _sqrt_recursion(int n)
 {
 
-	return (ssqrt(0, n));
+	return (find_sqrt(0, n));
 
 }
 /**
- * ssqrt - Search square a number.
- * @n: Value number n.
- * @i: Value number i.
- * Return: -1.
- */
-int ssqrt(int i, int n)
+  * find_sqrt - Calculates the sqrt of a number
+  * @i: Value
+  * @n: Value
+  * Return: -1.
+  */
+
+int find_sqrt(int i, int n)
 {
 
 	if (i * i == n)
-
 		return (i);
 	else if (i < n)
 		{
-		return (ssqrt(i + 1, n));
+		return (find_sqrt(i + 1, n));
 		}
 	else
 		return (-1);
