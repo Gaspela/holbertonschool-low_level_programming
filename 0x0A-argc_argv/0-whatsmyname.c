ce553535b0fc45dea72091cf861dc00cca6e45ee
Gaspela04 <samirmillanorozco@hotmail.com>
2019-10-13 21:54:17 +0000

    Argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints its name, followed by a new line.
+ * @argc: Number of command line arguments SB1
+ * @argv: Command line arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+
+}
---
+++ 0x0A-argc_argv/holberton.h
@@ -0,0 +1,8 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char c);
+int is_positive_number(char *number);
+int number_of_coins(int cents);
+
+#endif
