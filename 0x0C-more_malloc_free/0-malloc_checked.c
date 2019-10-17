881a7f47dcb232812eed84ea89f813f8df5dc291
Gaspela04 <samirmillanorozco@hotmail.com>
2019-10-17 15:58:47 +0000

    Mallocfree

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,1 +1,17 @@
-Desc
+#include <stdio.h>
+#include <stdlib.h>
+/*
+ * malloc_checked - Allocates memory using malloc.
+ *@b: Amount memory the array.
+ *Return: Allocate pointer in memory.
+*/
+void *malloc_checked(unsigned int b)
+{
+
+	void *p;
+	p = malloc(b);
+	if (p == NULL)
+	exit(98);
+	return (P);
+
+}
