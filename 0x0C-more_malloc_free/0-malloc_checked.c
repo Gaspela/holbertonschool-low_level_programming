ecdc6dd784aff0c0deb1ee7ef64477db0ac989e1
Gaspela04 <samirmillanorozco@hotmail.com>
2019-10-17 18:38:23 +0000

    Mallocfree

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-#include "holberton.h"
+
 /**
  * malloc_checked - Allocates memory using malloc.
  * @b: Amount memory the array.
--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,1 +1,30 @@
-Desc
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * _strlen - Function string.
+ * @s: Value string check.
+ * Return: String.
+ */
+int _strlen(char *s)
+{
+
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+		return (i);
+
+}
+/**
+ * string_nconcat - Concatenate two strings.
+ * 
+ *
+ *
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+	
+
+}
