4f81c6f291686e412461f17b63ec98befdfbc833
Gaspela04 <samirmillanorozco@hotmail.com>
2019-10-21 20:22:49 +0000

    Typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef Macro_god
+#define Macro_god
+/**
+ * struct dog - Elementos of dog.
+ * @name: Dog name.
+ * @age: Dog age.
+ * @owner: Dog owner.
+*/
+struct dog
+{
+
+	char *name;
+	float *age;
+	char *owner;
+
+};
+/**
+ * dog_t - New elements dog 
+*/
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
