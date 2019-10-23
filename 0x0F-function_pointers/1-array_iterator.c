f7dba9e31e14bd6c225be7228003d3e4255f2992
Gaspela04 <samirmillanorozco@hotmail.com>
2019-10-23 21:24:40 +0000

    Funtionp

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,12 +9,12 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 
-	int i;
+	size_t i;
 
-	if ((*action) == NULL || array == NULL)
+	if (action == NULL || array == NULL)
 		return;
 
 	for (i = 0; i < size; i++)
-		(*action)(array[i]);
+		action(array[i]);
 
 }
