e80269c354e7c03c2a4b0eb74106cce7c7fefe34
Gaspela04 <samirmillanorozco@hotmail.com>
2019-10-23 20:35:12 +0000

    Funtionp

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,14 +3,13 @@
  * print_name - Print a name.
  * @name: The name to print.
  * @f: Pointer the funtion.
- * @Return: 0;
+ * Return: 0;
  */
 void print_name(char *name, void (*f)(char *))
 {
 
 	if (f == NULL)
-	{
+		return;
 		(*f)(name);
-	}
 
 }
