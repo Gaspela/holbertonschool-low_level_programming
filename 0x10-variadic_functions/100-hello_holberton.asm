ab07895966207ff8d8da9e102642a4044597d778
Gaspela04 <samirmillanorozco@hotmail.com>
2019-10-26 21:18:52 +0000

    Valisto

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,12 +1,12 @@
 	global	main
 
 	section	.text
-main:	mov	rax, 1 /*Write*/
+main:	mov	rax, 1
 	mov	rdi, 1
-	mov	rsi, holberton /*Hello, Holberton*/
-	mov	rdx, 17 /*Sizeof*/
+	mov	rsi, holberton
+	mov	rdx, 17
 	syscall
-	mov	rax, 60 /*Exit*/
+	mov	rax, 60
 	xor	rdi, rdi
 	syscall
 
