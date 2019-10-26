93f8d7f91f8bd4c5959628415267a13172a02486
Gaspela04 <samirmillanorozco@hotmail.com>
2019-10-26 19:05:32 +0000

    Valist

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", ptr);
 		else
 			printf("(nil)");
-			if (separator && i < (n - 1))
+			if (separator && (i < n - 1))
 				printf("%s", separator);
 		}
 		printf("\n");
