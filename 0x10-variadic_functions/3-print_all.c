1a9d47daaca82cc105a815ba2159c1d27f3cb378
Gaspela04 <samirmillanorozco@hotmail.com>
2019-10-27 16:44:47 +0000

    Valist

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -54,13 +54,13 @@ void funtionprint_s(va_list s)
 void print_all(const char * const format, ...)
 {
 
-	funtions array[] = {{"c", funtionprint_c},
+	functions array[] = {{"c", funtionprint_c},
 	{"i", funtionprint_i},
 	{"f", funtionprint_f},
 	{"s", funtionprint_s},
 	{NULL, NULL}};
 
-	int i = 0, j = 0;
+	unsigned int i = 0, j = 0;
 	void (*funtion)(va_list);
 	va_list list;
 	char *spt = "";
@@ -70,9 +70,9 @@ void print_all(const char * const format, ...)
 	/*format is a list of types of arguments passed to the function*/
 		while (format && format[i])
 			{ j = 0;
-			while ((array[j]).s != NULL)
+			while (array[j].s != NULL)
 				{
-				if (array[i].s[0] == format[i])
+				if (array[j].s[0] == format[i])
 					{
 					printf("%s", spt);
 					funtion = array[j].p;
