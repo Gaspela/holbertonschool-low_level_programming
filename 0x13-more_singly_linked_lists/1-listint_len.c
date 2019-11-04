130310c58cec5e107f63cb804b399429701bea27
Gaspela04 <samirmillanorozco@hotmail.com>
2019-11-04 19:16:35 +0000

    Morelists

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,1 +1,22 @@
-Desc
+#include "lists.h"
+/**
+ * listint_len - Print list int.
+ * @h: head string list.
+ * Return: Number nodes.
+*/
+size_t listint_len(const listint_t *h)
+{
+/*unsigned int len = 0*/
+	size_t lennodes = 0;
+
+	while (h)
+	{
+/*Print value h*/
+		/*printf("%d\n", h->n);*/
+		h = h->next;
+		lennodes++;
+
+	}
+
+	return (lennodes);
+}
