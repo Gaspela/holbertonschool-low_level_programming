ef312e4906b6e750a06c52756b7a15794dc29940
Gaspela04 <samirmillanorozco@hotmail.com>
2019-11-05 21:01:20 +0000

    Morelists

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * add_nodeint - Adds a new node at the beginning of a listint_t.
+ * @head: Head list.
+ * @n: Value int.
+ * Return: Sdds a new node at the beginning of a listint_t.
+*/
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+
+	listint_t *node;
+	
+	if (!head)
+	return (NULL);
+
+	node = malloc(sizeof(listint_t));
+	if (!node)
+		return (NULL);
+
+	node->n = n;
+	node->next = *head;
+	*head = node;
+	
+	return (node);
+
+}
