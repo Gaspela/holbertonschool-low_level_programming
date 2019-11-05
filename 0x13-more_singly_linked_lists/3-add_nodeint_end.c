c17389bf109197bd5e0a251fcbc84478ae6224c7
Gaspela04 <samirmillanorozco@hotmail.com>
2019-11-05 21:36:56 +0000

    Morelists

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+ * add_nodeint_end - Adds a new node at the end of a listint_t.
+ * @head: head string list.
+ * @n: int.
+ * Return: Number nodes.
+*/
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+
+	listint_t *p;
+	
+	/**/
+	if(!head)
+		return (NULL);
+	/**/
+	if (*head)
+		return (add_nodeint_end(&(*head)->next, n));
+	
+	p = malloc(sizeof(listint_t));
+	
+	if(!p)
+	return (NULL);
+
+	p->n = n;
+	p->next = *head;
+	*head = p;
+
+		return (p);
+
+}
