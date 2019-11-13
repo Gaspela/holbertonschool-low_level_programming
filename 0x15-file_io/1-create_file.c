4dcf6c78f1cdebd2f79c827dba1957a218639b9b
Gaspela04 <samirmillanorozco@hotmail.com>
2019-11-13 02:07:52 +0000

    Fileio

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 			return (1);
 
 	while (text_content[i])
-			i++;
+		i++;
 	wr = write(fd, text_content, i);
 		if (wr == -1 || wr != i)
 
