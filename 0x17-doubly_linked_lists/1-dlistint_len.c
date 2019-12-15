26eb28beebdf3527dad96baa405156ef56be6d06
Gaspela04 <samirmillanorozco@hotmail.com>
2019-12-15 16:36:40 +0000

    Print nodes2

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * dlistint_len - Print all elements.
+ * @h: List in.
+ * Return: node.
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+
+	size_t node = 0;
+
+	while (h)
+	{
+	node++;
+	h = h->next;
+	}
+
+	return(node);
+
+}
