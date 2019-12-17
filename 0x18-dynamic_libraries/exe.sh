2bca5260e75bddbec7279bf12165846d77045785
Gaspela04 <samirmillanorozco@hotmail.com>
2019-12-17 02:49:37 +0000

    Dinamyc

--- 0x18-dynamic_libraries/exe.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cat list|cut -d "." -f1|
-while read CD_KND
- do
-   Filename=$(find ../ -name "${CD_KND}.c" | head -n1)
-   echo "creating ${CD_KND} file"
-   gcc -fPIC -c "${Filename}" -o "${CD_KND}.o"
-   gcc -shared *.o -o libholberton.so
-done
