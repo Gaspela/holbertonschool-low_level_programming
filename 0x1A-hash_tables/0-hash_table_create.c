18c05e8a1e539653a0698a4d5b236a255797baa1
Gaspela04 <samirmillanorozco@hotmail.com>
2020-01-18 23:20:03 +0000

    fix spaces

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,19 +6,18 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+hash_table_t *hash_table;
 
-	hash_table_t *hash_table;
+hash_table = malloc(sizeof(hash_table_t));
+if (!hash_table || !size)
+return (NULL);
 
-	hash_table = malloc(sizeof(hash_table_t));
-	if (!hash_table || !size)
-		return (NULL);
-
-	hash_table->array = calloc(size, sizeof(hash_node_t *));
-	/* if (!hash_table)
-	{
-		free(hash_table);
-		return (NULL);
-	} */
-	hash_table->size = size;
-	return (hash_table);
+hash_table->array = calloc(size, sizeof(hash_node_t *));
+if (!hash_table)
+{
+free(hash_table);
+return (NULL);
+}
+hash_table->size = size;
+return (hash_table);
 }
