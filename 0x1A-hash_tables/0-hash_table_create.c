cdb76c821d590c186cb6af95d3759ce54d3da0db
Gaspela04 <samirmillanorozco@hotmail.com>
2020-01-18 17:18:21 +0000

    Create hashtable

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - Create a hash table.
+ * @size: Nodes and collisions.
+ * Return: Return the *hash_table pointer direcction.
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+
+	hash_table_t *hash_table;
+
+	hash_table = malloc(sizeof(hash_table_t));
+	if (!hash_table || !size)
+	{
+		return (NULL);
+	}
+	hash_table->array = calloc(size, sizeof(hash_table_t *));
+	if (!hash_table)
+	{
+		/* code */
+		free(hash_table);
+		return (NULL);
+	}
+	hash_table->size = size;
+	return (hash_table);
+}
