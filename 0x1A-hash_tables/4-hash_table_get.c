1214b70fa5f6509e45396bdb72d9aa24c9607ae4
Gaspela04 <samirmillanorozco@hotmail.com>
2020-01-19 16:06:45 +0000

    fix new line end file

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -7,21 +7,21 @@
 */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    unsigned long int index;
-    hash_node_t *temp;
+	unsigned long int index;
+	hash_node_t *temp;
 
-    if (!ht || !key)
-        return (NULL);
+	if (!ht || !key)
+		return (NULL);
 
-    index = key_index((const unsigned char *)key, ht->size);
-    temp = ht->array[index];
+	index = key_index((const unsigned char *)key, ht->size);
+	temp = ht->array[index];
 
-    while (temp)
-    {
-        if (strcmp(temp->key, key) == 0)
-            return (temp->value);
-        temp = temp->next;
-    }
+	while (temp)
+	{
+		if (strcmp(temp->key, key) == 0)
+			return (temp->value);
+		temp = temp->next;
+	}
 
-    return (NULL);
+	return (NULL);
 }
