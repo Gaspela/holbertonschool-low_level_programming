6eac5ada7802dd92cb3b16cab4413ef1d2d83a27
Gaspela04 <samirmillanorozco@hotmail.com>
2020-01-19 22:49:43 +0000

    Fix

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -11,7 +11,7 @@ void hash_table_print(const hash_table_t *ht)
 
 	if (!ht)
 		return;
-		
+
 	printf("{");
 	while (i < ht->size)
 	{
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 /**
  * struct hash_node_s - Node of a hash table
