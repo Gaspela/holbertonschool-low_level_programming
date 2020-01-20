db4ead0c2d4e52a3b89d295a7c15fd01abb8d89e
Gaspela04 <samirmillanorozco@hotmail.com>
2020-01-20 03:09:47 +0000

    delete

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - Function that deletes a hash table.
+ * @ht: The hash table.
+*/
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *Point;
+    hash_node_t *temp;
+
+	while (i < ht->size)
+	{
+		Point = ht->array[i];
+		while (Point)
+		{
+			temp = Point;
+			Point = Point->next;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+		}
+		i++;
+	}
+    
+	free(ht->array);
+	free(ht);
+}
