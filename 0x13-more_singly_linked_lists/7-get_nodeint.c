48d74267130cfb53307930b92169e73616ac1107
Gaspela04 <samirmillanorozco@hotmail.com>
2020-03-16 16:44:01 +0000

    Complete topic

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index -function that returns the nth node of a listint_t linked list.
+ * @head: The list.
+ * @index: index of the node, starting at 0.
+ * Return: the node does not exist, return NULL.
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	if (!head)
+		return (NULL);
+
+	for (i = 1; i <= index; i++)
+	{
+		if (head->next)
+			head = head->next;
+		else
+			return (NULL);
+	}
+
+	return (head);
+}
---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * sum_listint - function that returns the sum of all the data (n) of a listint_t linked list.
+ * @head: The list.
+ * Return: the list is empty, return 0.
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+
+		if (head->next)
+			head = head->next;
+		else
+			break;
+	}
+
+	return (sum);
+}
