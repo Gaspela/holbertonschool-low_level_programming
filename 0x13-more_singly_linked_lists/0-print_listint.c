1f067fa4b4db2e6f149d3b36322bbffba252ca04
Gaspela04 <samirmillanorozco@hotmail.com>
2020-03-16 18:09:31 +0000

    Complete topic

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,16 +6,15 @@
 */
 size_t print_listint(const listint_t *h)
 {
-/*unsigned int len = 0*/
+
 	size_t lennodes = 0;
 
 	while (h)
 	{
-/*Print value h*/
+
 		printf("%d\n", h->n);
 		h = h->next;
 		lennodes++;
-
 	}
 
 	return (lennodes);
