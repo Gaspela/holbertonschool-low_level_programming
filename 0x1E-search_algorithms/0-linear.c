1c98726e605147ae75cfed2096d5a323b8a01653
Samir millan orozco <samirmillanorozco@hotmail.com>
2020-04-15 18:45:19 +0000

    Betty

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,13 +9,13 @@
 */
 int linear_search(int *array, size_t size, int value)
 {
-    unsigned int i;
+	unsigned int i;
 
-    if (!array)
-        return (-1);
-    
-    for (i = 0; i < size; i++)
-    {
+	if (!array)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
